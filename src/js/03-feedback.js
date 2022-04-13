2f6506409bf31f310ddfc4c0e087f9f243d4b7ac
Imavin08 <92112933+Imavin08@users.noreply.github.com>
2022-04-13 23:08:51 +0200

    task 1/2/3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const formValue = {};
+const STORAGE_KEY = 'feedback-form-state';
 
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput() {
+  formValue.email = form.email.value;
+  formValue.message = form.message.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formValue));
+}
+
+function inputLocalStorage() {
+  const storageValue = localStorage.getItem(STORAGE_KEY);
+  const parce = JSON.parse(storageValue);
+  if (parce) {
+    form.email.value = parce.email;
+    form.message.value = parce.message;
+  }
+}
+inputLocalStorage();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  localStorage.removeItem(STORAGE_KEY);
+  const formValue = {
+    email: e.currentTarget.email.value,
+    message: e.currentTarget.message.value,
+  };
+  console.log(formValue);
+  form.reset();
+}
