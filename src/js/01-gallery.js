787c876967174a98d4643c38cb1ddc5c188b86b5
Imavin08 <92112933+Imavin08@users.noreply.github.com>
2022-04-13 21:02:47 +0200

    task 1 - 2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function createGallery({ preview, original, description }) {
+  return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}
+
+function addGallery(value) {
+  const gallery = value.map(createGallery).join('');
+  galleryContainer.innerHTML = gallery;
+
+  makeNewGallery();
+}
+
+function makeNewGallery() {
+  new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
+
+addGallery(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(setTime, 1000));
+
+function setTime(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
